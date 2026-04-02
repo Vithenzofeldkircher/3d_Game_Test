ff5cff80659aef176ab70507a690fbe2642a2610
Vithenzofeldkircher <vithenzo.feldkircher@alunos.sc.senac.br>
2026-04-02 10:49:01 -0300

    Update

---
+++ Assets/Script/Gun.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    private Transform _camera;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _camera = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(_camera.position, _camera.forward))
+        {
+            print("Tiro");
+        }
+    }
+}
